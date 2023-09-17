e194f2c5848c349caa0918872901c929974583fa
shuaify <shuaiyuan4@gmail.com>
2023-09-17 10:22:55 -0500

    Initial commit

---
+++ setup.r
@@ -0,0 +1,13 @@
+library(usethis)
+library(devtools)
+packageVersion("devtools")
+
+
+use_git_config(
+    user.name ="shuaify",
+    user.email="shuaiyuan4@gmail.com"
+)
+
+git_sitrep()
+
+use_git()
