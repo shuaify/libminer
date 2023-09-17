5a752dfc63eef8d5ef9172a0ea6d90c353a16fbc
shuaify <shuaiyuan4@gmail.com>
2023-09-17 10:42:11 -0500

    initial setup

--- setup.r
+++ setup.r
@@ -2,12 +2,16 @@ library(usethis)
 library(devtools)
 packageVersion("devtools")
 
-
+# git config
 use_git_config(
     user.name ="shuaify",
     user.email="shuaiyuan4@gmail.com"
 )
 
+# check git status
 git_sitrep()
 
+# use git
 use_git()
+
+
